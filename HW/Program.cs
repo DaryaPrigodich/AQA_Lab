0c40e31236f6c0c922c0dedc0baef8e3b4ced87f
David Rabko <davidryabko13@gmail.com>
2022-11-14 13:05:05 +0100

    Add review notes

--- HW/Program.cs
+++ HW/Program.cs
@@ -7,14 +7,15 @@ Console.WriteLine("Введите сумму обмена:");
 float sum = Convert.ToSingle(Console.ReadLine());
 
 Console.WriteLine("Введите валюту 1:");
+// старайся использовать более понятные названия переменных, например val1 -> userCurrency, val2 -> exchangeCurrency
 string val1 = Console.ReadLine();
 
 Console.WriteLine("Введите валюту 2:");
 string val2 = Console.ReadLine();
 
-if (val1 == "USD" && val2 == "EUR") 
+if (val1 == "USD" && val2 == "EUR") // старайся не хардкодить строки, используй переменные: var usdCurrency = "USD" ...
 {
-    float a = sum * 0.96f;
+    float a = sum * 0.96f; // запрети себе называть перменную a, b, c ... , имя переменной должно говорить о ее применении
     Console.WriteLine($"{a} EUR");
 }
 else if (val1 == "USD" && val2 == "ZL") 
