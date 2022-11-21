280d4e23697bb46fd3d9f545b484984661b3f0ca
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2022-11-21 15:27:23 +0300

    converter with constructor
    (cherry picked from commit c95a3b6f4b185e5d69f22091eafbde896152a8b1)

---
+++ convert/Currencies.cs
@@ -0,0 +1,8 @@
+﻿namespace convert;
+
+public class Currencies
+{
+    public const string USD = "USD";
+    public const string EUR = "EUR";
+    public const string ZL = "ZL";
+}
---
+++ convert/converter.cs
@@ -0,0 +1,50 @@
+﻿
+Converter converter = new  Converter(1000M,"USD","ZL",0.22M,0.46M,0.33M,0.11M,0.66M,0.44M);
+Console.WriteLine(converter.Convert());
+
+public class Converter
+{
+    public decimal sumCurrency;
+    public string userCurrency;
+    public string exchangeCurrency;
+    
+    public decimal UsdEur;
+    public decimal UsdZl;
+    public decimal EurUsd;
+    public decimal EurZl;
+    public decimal ZlUsd;
+    public decimal ZlEur;
+    public Converter(decimal sumCurrency, string userCurrency, string exchangeCurrency, decimal UsdEur, decimal UsdZl, decimal EurUsd,decimal EurZl,decimal ZlUsd,decimal ZlEur)
+    {
+        this.sumCurrency = sumCurrency;
+        this.userCurrency = userCurrency;
+        this.exchangeCurrency = exchangeCurrency;
+
+        this.UsdEur = UsdEur;
+        this.UsdZl = UsdZl;
+        this.EurUsd = EurUsd;
+        this.EurZl = EurZl;
+        this.ZlUsd = ZlUsd;
+        this.ZlEur = ZlEur;
+  
+    }
+    public decimal Convert()
+    {
+    
+        switch (userCurrency, exchangeCurrency)
+        {
+            case ("USD","EUR"): return sumCurrency * UsdEur;
+                break;
+            case ("USD","ZL"): return sumCurrency * UsdZl;
+                break;
+            case ("EUR","USD"): return sumCurrency * EurUsd;
+                break;
+            case ("EUR","ZL"): return sumCurrency * EurZl;
+                break;
+            case ("ZL","USD"): return sumCurrency * ZlUsd;
+                break;
+            default: return sumCurrency * ZlEur;
+                break;
+        }
+    }
+}
