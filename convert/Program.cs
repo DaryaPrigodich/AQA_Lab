89ed52106ae9ab365c90a4d636b6e35f51930ec4
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2022-12-08 11:53:42 +0300

    Fix review notes

--- convert/Program.cs
+++ convert/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
    public static void Main(string[] args)
    {
-      var converter = new Converter(0.22M,4.6M,0.33M,0.11M,0.66M,0.44M);
+      var converter = new Converter(0.22M,4.6M,0.33M,0.11M,0.66M, 1.22M);
       var result = converter.Convert(1000M, Currencies.Usd, Currencies.Zl);
       Console.WriteLine(result);
    }
