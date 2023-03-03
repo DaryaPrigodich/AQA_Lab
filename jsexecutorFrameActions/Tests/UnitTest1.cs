0b3a00788dd2ce7744e35a60d3fad3b81f91d965
Darya <104024169+DaryaPrigodich@users.noreply.github.com>
2023-03-03 23:01:45 +0300

    Set Up Allure report

--- jsexecutorFrameActions/Tests/UnitTest1.cs
+++ jsexecutorFrameActions/Tests/UnitTest1.cs
@@ -1,11 +1,20 @@
+using Allure.Commons;
 using jsexecutorFrameActions.BaseEntities;
 using jsexecutorFrameActions.Pages;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
 
 namespace jsexecutorFrameActions;
 
+[TestFixture]
+[AllureNUnit]
 public class Tests : BaseTest
 {
     [Test]
+    [AllureTag("Smoke")]
+    [AllureSeverity(SeverityLevel.critical)]
+    [AllureOwner("Darya")]
+    [AllureSuite("Search engine Feature")]
     public void Test1()
     {
         var catalogPage = new CatalogPage(Driver, true);
